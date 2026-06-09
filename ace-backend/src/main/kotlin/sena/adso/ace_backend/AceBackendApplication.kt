8f5515b4d981da4cd6d202d3b492bc98ce4a390b
reinaldo <rjperalta07@soy.sena.edu.co>
2026-06-08 20:16:29 -0500

    chore: remove ace-shared to separate repo
    ace-shared now lives in https://github.com/stevenaraque/ace-shared
    Consumed via JitPack: com.github.stevenaraque:ace-shared:1.0.0

--- ace-backend/src/main/kotlin/sena/adso/ace_backend/AceBackendApplication.kt
+++ ace-backend/src/main/kotlin/sena/adso/ace_backend/AceBackendApplication.kt
@@ -1,14 +1,11 @@
 package sena.adso.ace_backend
 
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
-import org.springframework.context.annotation.Import
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-@Import(FlywayAutoConfiguration::class)
+@SpringBootApplication  // sin exclude
 class AceBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<AceBackendApplication>(*args)
+    runApplication<AceBackendApplication>(*args)
 }
