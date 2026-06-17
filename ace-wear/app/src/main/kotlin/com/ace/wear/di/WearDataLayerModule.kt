07571efbfeb3800eb2b0fdf27be5c3e375371b15
unknown <stevenalejandroaraquecastro@gmail.com>
2026-06-17 09:20:55 -0500

    arreglo de la aplicacion de wear os

--- ace-wear/app/src/main/kotlin/com/ace/wear/di/WearDataLayerModule.kt
+++ ace-wear/app/src/main/kotlin/com/ace/wear/di/WearDataLayerModule.kt
@@ -5,6 +5,7 @@ package com.ace.wear.di
 import android.content.Context
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.MessageClient
+import com.google.android.gms.wearable.NodeClient
 import com.google.android.gms.wearable.Wearable
 import dagger.Module
 import dagger.Provides
@@ -34,5 +35,13 @@ object WearDataLayerModule {
         @ApplicationContext context: Context
     ): MessageClient {
         return Wearable.getMessageClient(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNodeClient(
+        @ApplicationContext context: Context
+    ): NodeClient {
+        return Wearable.getNodeClient(context)
     }
 }
